6e3028c974f8f824927820496d28fb37276fc615
sufiya-begum <sufiyabegum984@gmail.com>
2026-01-31 10:47:42 +0530

    Time: 3 ms (52.37%) | Memory: 7.8 MB (44.78%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.c
+++ 1013-fibonacci-number/fibonacci-number.c
@@ -1,11 +1,7 @@
 
 int fib(int n){
-      if(n==0){
-        return 0;
-      }
-      if(n==1){
-        return 1;
-      }
+      if(n<1)
+      return n;
       int a=0,b=1,c;
       for(int i=2;i<=n;i++){
         c=a+b;
